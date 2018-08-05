0a0a50ee035c67a083c8e7b9a85bbe7c125adcd1
SirSerje <sir.serje@gmail.com>
2018-08-05 14:26:36 +0300

    add lodash, change data parse, update RenderList

--- fe/src/components/search/RenderList.js
+++ fe/src/components/search/RenderList.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import uuidv4 from 'uuid'
-import { Badge, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import isNil from 'lodash/isNil'
 
 // FIXME: when data will change to actual, update parsing
 const RenderList = props => {
   let list = props.data
-    return (
-      <ListGroup>
-        {list.values.map(e => (
-          <ListGroupItem href="" key={uuidv4()}>
-            <Badge>{'category'}</Badge>  {e.id} | {e.val}
+  if (isNil(list.categories) || isNil(list.items)) {
+    return <b>sorry no results</b>
+  }
+  return (
+    <ListGroup id={'render-list'}>
+      {list.categories.map(e => (
+        <ListGroupItem bsStyle="info" href="" key={uuidv4()}>
+          {`${e.name} (${e.hits})`}
           </ListGroupItem>
-        ))}
-      </ListGroup>
-    )
+      ))}
+      {list.items.map(e => (
+        <ListGroupItem href="" key={uuidv4()}>
+          {`${e.name}`}
+          </ListGroupItem>
+      ))}
+    </ListGroup>
+  )
 }
 
 export default RenderList
