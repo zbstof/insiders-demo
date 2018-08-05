026f01b9ec2365a318081ef6a62568f89594f1f5
SirSerje <sir.serje@gmail.com>
2018-08-05 14:44:04 +0300

    update render list

--- fe/src/components/search/RenderList.js
+++ fe/src/components/search/RenderList.js
@@ -1,25 +1,40 @@
 import React from 'react'
 import uuidv4 from 'uuid'
-import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Image, ListGroup, ListGroupItem } from 'react-bootstrap'
 import isNil from 'lodash/isNil'
 
 // FIXME: when data will change to actual, update parsing
 const RenderList = props => {
   let list = props.data
   if (isNil(list.categories) || isNil(list.items)) {
-    return <b>sorry no results</b>
+    return <p />
   }
+  if (list.categories.length < 1 && list.items.length < 1)
+    return (
+      <p>
+        Your search has no results. Probably, we will make this page looks
+        better
+      </p>
+    )
+
+  if (list.items.length < 1) return <p>Looks like, there is no items</p>
+
   return (
     <ListGroup id={'render-list'}>
       {list.categories.map(e => (
         <ListGroupItem bsStyle="info" href="" key={uuidv4()}>
           {`${e.name} (${e.hits})`}
-          </ListGroupItem>
+        </ListGroupItem>
       ))}
       {list.items.map(e => (
         <ListGroupItem href="" key={uuidv4()}>
-          {`${e.name}`}
-          </ListGroupItem>
+          <Image
+            src={'http://retrogamer.biz/wp-content/uploads/2015/09/navody.png'}
+            width={16}
+            height={16}
+          />
+          {` ${e.name}`}
+        </ListGroupItem>
       ))}
     </ListGroup>
   )
