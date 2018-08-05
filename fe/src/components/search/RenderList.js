b4cfb57b88f3a42dffeaa119c03cdc08b7070290
SirSerje <sir.serje@gmail.com>
2018-08-05 15:53:58 +0300

    add click for local storage behavior

--- fe/src/components/search/RenderList.js
+++ fe/src/components/search/RenderList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import uuidv4 from 'uuid'
 import { Image, ListGroup, ListGroupItem } from 'react-bootstrap'
 import isNil from 'lodash/isNil'
+import getImage from '../../api/getImage'
 
 // FIXME: when data will change to actual, update parsing
 const RenderList = props => {
@@ -22,22 +23,32 @@ const RenderList = props => {
   return (
     <ListGroup id={'render-list'}>
       {list.categories.map(e => (
-        <ListGroupItem bsStyle="info" href="" key={uuidv4()}>
+        <ListGroupItem bsStyle="info" href="/" key={uuidv4()} onClick={onClick}>
           {`${e.name} (${e.hits})`}
         </ListGroupItem>
       ))}
       {list.items.map(e => (
-        <ListGroupItem href="" key={uuidv4()}>
-          <Image
-            src={'http://retrogamer.biz/wp-content/uploads/2015/09/navody.png'}
-            width={16}
-            height={16}
-          />
+        <ListGroupItem href="/" key={uuidv4()} onClick={onClick}>
+          <Image src={getImage} width={16} height={16} />
           {` ${e.name}`}
         </ListGroupItem>
       ))}
     </ListGroup>
   )
+}
+
+const onClick = e => {
+  // TODO: add hit counter
+  e.preventDefault()
+  let storage = []
+  const KEY = 'insider4'
+  if (JSON.parse(localStorage.getItem(KEY)) !== null) {
+    storage = JSON.parse(localStorage.getItem(KEY))
+  }
+  if (!isNil(e) && !isNil(e.target) && !isNil(e.target.innerText)) {
+    storage.push(e.target.innerText)
+  }
+  localStorage.setItem(KEY, JSON.stringify(storage))
 }
 
 export default RenderList
